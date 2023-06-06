31db7c00eca7a40d7806725ef6b2732da5ee7407
JulianD3wanto <juliandewanto@student.uns.ac.id>
2023-06-06 20:27:08 +0700

    Tambahan Migration

--- database/migrations/2023_05_30_132625_create_pembicara_table.php
+++ database/migrations/2023_05_30_132625_create_pembicara_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pembicara', function (Blueprint $table) {
-            $table->uuid("idPembicara")->primary();
+            $table->uuid("idPembicara");
             $table->string("namaPembicara");
             $table->string("asalPembicara");
 
