be0899821149f5268e3ba2c64ce0417de4c154cd
Muflihsan <muflihihsanpratama@gmail.com>
2023-06-13 20:34:00 +0700

    salah tolong jangan ditiru

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Role::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $data = [
+            ['name' => 'Admin'],
+            ['name' => 'Peserta'],
+            ['name' => 'PIC'],
+        ];
+
+        foreach ($data as $value) {
+            Role::insert([
+                'name' => $value['name'],
+            ]);
+        }
+    }
+}
